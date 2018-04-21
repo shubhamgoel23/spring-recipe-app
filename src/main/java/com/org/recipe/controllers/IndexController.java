2b0aeb4f3523f7fce56d9f88ccf792de4c27bed4
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-22 00:50:31 +0530

    added recipe and notes entity classes

--- src/main/java/com/org/recipe/controllers/IndexController.java
+++ src/main/java/com/org/recipe/controllers/IndexController.java
@@ -9,7 +9,6 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-
         return "index";
     }
 }
---
+++ src/main/java/com/org/recipe/domain/Notes.java
@@ -0,0 +1,50 @@
+package com.org.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Notes{" +
+                "id=" + id +
+                ", recipe=" + recipe +
+                ", recipeNotes='" + recipeNotes + '\'' +
+                '}';
+    }
+}
