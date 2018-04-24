198f2fcb0d97ffbfe118c50cbaf40bee506a4777
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-25 00:06:40 +0530

    implemented project lombok annotations and bootstrap cdn

--- src/main/java/com/org/recipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/org/recipe/bootstrap/RecipeBootstrap.java
@@ -4,9 +4,11 @@ import com.org.recipe.domain.*;
 import com.org.recipe.repositories.CategoryRepository;
 import com.org.recipe.repositories.RecipeRepository;
 import com.org.recipe.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.Optional;
 
 /**
  */
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -29,8 +32,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("loading bootstrap data");
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/com/org/recipe/domain/Ingredient.java
+++ src/main/java/com/org/recipe/domain/Ingredient.java
@@ -1,12 +1,14 @@
 package com.org.recipe.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
 @Entity
+@EqualsAndHashCode(exclude = {"recipe"})
 public class Ingredient {
 
     @Id
