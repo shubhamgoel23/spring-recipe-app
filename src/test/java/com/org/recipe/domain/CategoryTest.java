1826b0ed8765cc3a94ca2cdf34c31a3ea631c28a
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-28 13:07:56 +0530

    junit using mockito mocks

---
+++ src/test/java/com/org/recipe/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package com.org.recipe.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4l;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
