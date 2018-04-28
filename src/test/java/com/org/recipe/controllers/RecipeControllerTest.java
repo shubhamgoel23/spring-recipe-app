7e805676cbb8312bacfa89288a3b7cf0e592be6e
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-28 21:46:34 +0530

    displaying recipe details by id

---
+++ src/test/java/com/org/recipe/controllers/RecipeControllerTest.java
@@ -0,0 +1,48 @@
+package com.org.recipe.controllers;
+
+import com.org.recipe.domain.Recipe;
+import com.org.recipe.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        this.controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void showById() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        ResultActions perform = mockMvc.perform(get("/recipe/show/1"));
+        perform.andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+
+    }
+
+}
