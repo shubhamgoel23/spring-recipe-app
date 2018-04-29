f505947c2dc9594b126b9928cb3caa57eedacea4
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-04-29 15:39:08 +0530

    modified notes setter method

--- src/main/java/com/org/recipe/domain/Recipe.java
+++ src/main/java/com/org/recipe/domain/Recipe.java
@@ -50,8 +50,10 @@ public class Recipe {
     private Date updateTimestamp;
 
     public void setNotes(Notes notes) {
-        this.notes = notes;
-        notes.setRecipe(this);
+        if (notes != null) {
+            this.notes = notes;
+            notes.setRecipe(this);
+        }
     }
 
     public Recipe addIngredients(Ingredient ingredient){
