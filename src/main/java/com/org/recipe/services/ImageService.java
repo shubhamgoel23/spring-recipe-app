40a1a9a41be24892c6aaded22adfff7d6b4d5382
Shubham Goel <shubhamgoyal23@yahoo.com>
2018-05-14 00:14:39 +0530

    persisting images to database

---
+++ src/main/java/com/org/recipe/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.org.recipe.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
